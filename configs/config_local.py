f4f25edf085491488aed49d16bd43bddc0f6b801
Arthur <104057511+arthuq@users.noreply.github.com>
2023-04-07 14:06:12 +0200

    up

--- configs/config_local.py
+++ configs/config_local.py
@@ -2,41 +2,49 @@
 Arthur MARON
 2023
 """
-"""
+
+import os
 import sys
 import requests
 import traceback
 import time
 import json
-import sys
-import os
+
 import pandas as pd
-from datetime import datetime, timedelta
+import numpy as np
 import matplotlib.pyplot as plt
+
+from datetime import datetime, timedelta
 from cryptocmd import CmcScraper
-"""
 
-#PATHS
-global project_path, config_path, src_path, dat_path, csv_path,fig_path, version_path, neg_corpus_path, pos_corpus_path, version
+from scipy import stats
+from numpy.random import default_rng
+rng = default_rng()
+import torch
+import torch.nn as nn
 
-project_path = "D:/3.Cours EK/8. SEMESTRE DEUX/4. CRYPTO/PROJECT"
+##
 
-config_path = project_path + "/configs"
 
-src_path = project_path + "/src"
+global PROJECT_PATH, CONFIG_PATH, SRC_PATH, DAT_PATH, CSV_PATH, FIG_PATH, TXT_PATH, VERSION_PATH, NEG_CORPUS_PATH, POS_CORPUS_PATH, VERSION, NEXT_VERSION
 
-dat_path = project_path + "/src/dat"
+PROJECT_PATH = "D:/3.Cours EK/8. SEMESTRE DEUX/4. CRYPTO/PROJECT"
 
-csv_path = src_path + "/csv"
-fig_path = src_path + "/fig"
+CONFIG_PATH = PROJECT_PATH + "/configs"
+SRC_PATH = PROJECT_PATH + "/src"
+DAT_PATH = PROJECT_PATH + "/src/dat"
+CSV_PATH = SRC_PATH + "/csv"
+FIG_PATH = SRC_PATH + "/fig"
+TXT_PATH = SRC_PATH + "/txt_tmp"
 
-version_path = dat_path + "/_version.txt"
-neg_corpus_path = dat_path + "/_neg_corpus.txt"
-pos_corpus_path = dat_path + "/_pos_corpus.txt"
+VERSION_PATH = DAT_PATH + "/_version.txt"
+NEG_CORPUS_PATH = DAT_PATH + "/_neg_corpus.txt"
+POS_CORPUS_PATH = DAT_PATH + "/_pos_corpus.txt"
 
-with open(version_path, 'r') as f:
+with open(VERSION_PATH, 'r') as f:
     try :
         last_line = f.readlines()[-1]
     except:
         last_line = "0"
-    version = str(int(last_line.split(",")[0]))
+    VERSION = str(int(last_line.split(",")[0]))
+    NEXT_VERSION = str(1 + int(last_line.split(",")[0]))
